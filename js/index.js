b84dc113bc2d162c488acb4f509b03573789a92c
Jasur545 <jasursaitmuratov@gmail.com>
2023-02-25 10:50:26 +0500

    first commit

---
+++ js/index.js
@@ -0,0 +1,27 @@
+// const country = "uzbekiston";
+
+// console.log(typeof country);
+
+// let population = 35;
+
+// console.log(typeof population);
+
+// const continenti = "asia";
+
+// console.log(typeof continenti);
+
+// prompt((userAge = "Yoshingizni kiriting"));
+
+// if (userAge >= 18) {
+//   console.log("Sizga yaqin daqiqada guvohnoma beriladi");
+// } else {
+//   console.log(userAge - 18);
+// }
+
+const userName = prompt("Ismingizdi kiriting");
+
+if (userName == "student") {
+  console.log(`${userName} siz yana o'qishingiz kerak`);
+} else {
+  console.log("Siz yaxshi ishlang");
+}
